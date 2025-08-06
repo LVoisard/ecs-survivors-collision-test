bf8bdaf804f6a39569f14c6fc4be2febc05866ae
ubuntu <you@example.com>
2025-08-06 13:18:46 -0400

    gonna perform the test on a non-atom computer because this is bs

---
+++ src/perf_recorder.h
@@ -0,0 +1,12 @@
+//
+// Created by ubuntu on 2025-08-06.
+//
+
+#ifndef ECS_SURVIVORS_PERF_RECORDER_H
+#define ECS_SURVIVORS_PERF_RECORDER_H
+
+
+class perf_recorder {};
+
+
+#endif // ECS_SURVIVORS_PERF_RECORDER_H
